a35dc67b30426f02be7a0c47cee4c22be2e9cc28
bovarysme <bovarysme@users.noreply.github.com>
2018-06-04 15:34:14 +0200

    Implement CPU profiling

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime/pprof"
 
 	"github.com/bovarysme/memories/crypto"
 )
 
 var bruteforce bool
+var cpuprofile string
 var source, dest, ourID, theirID string
 
 func init() {
 	flag.BoolVar(&bruteforce, "bruteforce", false, "perform a key-recovery attack on the input file")
+	flag.StringVar(&cpuprofile, "cpuprofile", "", "write a CPU profile")
 	flag.StringVar(&source, "source", "", "path to the input file (e.g. chat-1067048330)")
 	flag.StringVar(&dest, "dest", "", "path to the output file")
 	flag.StringVar(&ourID, "oid", "", "your MID (e.g. u61726520762e206375746520f09f929c)")
@@ -48,6 +52,20 @@ func cmdDecrypt() error {
 }
 
 func main() {
+	if cpuprofile != "" {
+		file, err := os.Create(cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = pprof.StartCPUProfile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer pprof.StopCPUProfile()
+	}
+
 	var err error
 	if bruteforce {
 		err = cmdBruteforce()
